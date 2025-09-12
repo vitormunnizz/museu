ac9facd23cc0b88db57d97b87c8872c91ef917dd
vitormunnizz <vitormunnizz@gmail.com>
2025-09-12 08:00:07 -0300

    Primeiro commit - adicionando projeto museu

---
+++ script.js
@@ -0,0 +1,55 @@
+const back = document.getElementById("back")
+const next = document.getElementById("next")
+
+const conteudo = document.querySelectorAll(".conteudo")
+
+const traco = document.querySelectorAll(".traco")
+
+
+let timer;
+let ativoConteudo = 0;
+let ativoTraco = 0;
+
+function mudar_pagina(i) {
+    conteudo[ativoConteudo].classList.remove("ativo");
+
+    ativoConteudo += i;
+
+    if (ativoConteudo >= conteudo.length) {
+        ativoConteudo = 0;
+    } else if (ativoConteudo < 0) {
+        ativoConteudo = conteudo.length - 1;
+    }
+
+    conteudo[ativoConteudo].classList.add("ativo");
+}
+
+function mudar_traco(i) {
+    traco[ativoTraco].classList.remove("ativo");
+
+    ativoTraco += i;
+
+    if (ativoTraco >= traco.length) {
+        ativoTraco = 0;
+    } else if (ativoTraco < 0) {
+        ativoTraco = traco.length - 1;
+    }
+
+    traco[ativoTraco].classList.add("ativo");
+}
+
+
+back.addEventListener('click', () => {
+    mudar_pagina(-1)
+    mudar_traco(-1)
+})
+
+next.addEventListener('click', () => {
+    mudar_pagina(1)
+    mudar_traco(1)
+})
+
+clearInterval(timer)
+timer = setInterval(() => {
+    mudar_pagina(1)
+}, 30000);
